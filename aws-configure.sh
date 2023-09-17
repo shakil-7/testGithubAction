03eb823749252dc3c0e1477d8f7e1c8a0e1b561d
shakil-7 <132076841+shakil-7@users.noreply.github.com>
2023-09-17 15:41:59 +0600

    see if aws configure successfully

--- aws-configure.sh
+++ aws-configure.sh
@@ -11,3 +11,5 @@ aws configure set aws_access_key_id "fakeMyKeyId"
 aws configure set aws_secret_access_key "fakeMyKeyId"
 aws configure set default_region_name "ap-southeast-1"
 aws configure set default_output_format "json"
+
+aws configure list
