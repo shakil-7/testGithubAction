3e1d4ff64a445f0d3c25c9ebed59f1a4c9513f4d
mahadi-hasan <07shakilhasan@gmail.com>
2023-09-17 16:37:17 +0600

    add script to run test

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,4 +1,10 @@
-port="8088"
+port="9999"
 
 mvn test
-exit
+
+pid=$(lsof -i :$port | grep "java" | awk '{print $2}' | grep '^[0-9]\+$')
+echo $pid
+
+for process in $pid; do
+	kill -9 $process
+done
