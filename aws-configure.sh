dcf1d942f813c02326d33c5909a334e90690fc28
shakil-7 <132076841+shakil-7@users.noreply.github.com>
2023-09-17 15:38:04 +0600

    configure aws

--- aws-configure.sh
+++ aws-configure.sh
@@ -4,6 +4,9 @@ mkdir -p ~/.aws
 sudo touch ~/.aws/credentials
 sudo touch  ~/.aws/config
 
+chmod 600 ~/.aws/credentials
+chmod 600 ~/.aws/config
+
 aws configure set aws_access_key_id "fakeMyKeyId"
 aws configure set aws_secret_access_key "fakeMyKeyId"
 aws configure set default_region_name "ap-southeast-1"
