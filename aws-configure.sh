ca48cdb9a58617ff6469ee6996c3008a007a0ea4
shakil-7 <132076841+shakil-7@users.noreply.github.com>
2023-09-17 15:45:36 +0600

    Update aws-configure.sh

--- aws-configure.sh
+++ aws-configure.sh
@@ -9,7 +9,10 @@ chmod 600 ~/.aws/config
 
 aws configure set aws_access_key_id "fakeMyKeyId"
 aws configure set aws_secret_access_key "fakeMyKeyId"
-aws configure set default_region_name "ap-southeast-1"
+# aws configure set default_region_name "ap-southeast-1"
 aws configure set default_output_format "json"
+aws configure set region ap-southeast-1 --profile default
+aws configure set profile default
+
 
 aws configure list
