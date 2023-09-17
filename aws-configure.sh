5fa40c3e6e3e4adff47861e922528627f3808fae
shakil-7 <132076841+shakil-7@users.noreply.github.com>
2023-09-17 15:18:54 +0600

    Create aws-configure.sh

---
+++ aws-configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws configure set aws_access_key_id "fakeMyKeyId"
+aws configure set aws_secret_access_key "fakeMyKeyId"
+aws configure set default_region_name "ap-southeast-1"
+aws configure set default_output_format "json"
